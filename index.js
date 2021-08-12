dd2ba92f2a0215cc0d4781a398978a3318b445ce
Dan <DanielMooreRealPerson@gmail.com>
2021-08-11 21:28:42 -0400

    commit tree ae5d83d4321c0d4da57755d353b02783abb87fee

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    return`This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+const wrapAdjective = function (param='*') {
+    return function (innerParam='special') {
+        return (`You are ${param}${innerParam}${param}!`)
+    }
+    
+}
+
+let Calculator = {
+    add: () => 1+3,
+    subtract: () => 1-3,
+    multiply: () => 1*3,
+    divide: () => 10/5
+}
+
+function actionApplyer(startingInteger, arrayOfFunctions) {
+    let x = startingInteger
+
+    for (let y of arrayOfFunctions) {
+        x = y(x)
+    }
+    return x
+}
